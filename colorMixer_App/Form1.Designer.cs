f9cdbd73b672d1186902d299ffe7db10b3dd4be8
Shyam <rsambhetwal@gmail.com>
2023-01-23 01:17:45 +0200

    Function added to display the R,G,B color values of particular place

--- colorMixer_App/Form1.Designer.cs
+++ colorMixer_App/Form1.Designer.cs
@@ -35,24 +35,24 @@
             this.label3 = new System.Windows.Forms.Label();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlSelectedScreen1 = new System.Windows.Forms.Panel();
             this.tBxBlue = new System.Windows.Forms.MaskedTextBox();
             this.tBxGreen = new System.Windows.Forms.MaskedTextBox();
-            this.tBxred = new System.Windows.Forms.MaskedTextBox();
+            this.tBxRed = new System.Windows.Forms.MaskedTextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.tBxBlue1 = new System.Windows.Forms.MaskedTextBox();
             this.tBxGreen1 = new System.Windows.Forms.MaskedTextBox();
             this.tBxRed1 = new System.Windows.Forms.MaskedTextBox();
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.groupBox1.SuspendLayout();
@@ -68,7 +68,7 @@
             this.label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
             this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(141, 28);
+            this.label1.Size = new System.Drawing.Size(169, 33);
             this.label1.TabIndex = 0;
             this.label1.Text = "Color Mixer App\r\n";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -78,10 +78,11 @@
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(55, 102);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(300, 160);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.Size = new System.Drawing.Size(254, 159);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
             // 
             // label2
             // 
@@ -89,7 +90,7 @@
             this.label2.Font = new System.Drawing.Font("Sitka Display", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label2.Location = new System.Drawing.Point(96, 54);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(119, 23);
+            this.label2.Size = new System.Drawing.Size(151, 29);
             this.label2.TabIndex = 2;
             this.label2.Text = "Select First Color";
             this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -100,7 +101,7 @@
             this.label3.Font = new System.Drawing.Font("Sitka Display", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label3.Location = new System.Drawing.Point(525, 54);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(135, 23);
+            this.label3.Size = new System.Drawing.Size(172, 29);
             this.label3.TabIndex = 3;
             this.label3.Text = "Select Second Color";
             this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -117,10 +118,10 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.pnlSelectedScreen1);
             this.groupBox1.Controls.Add(this.tBxBlue);
             this.groupBox1.Controls.Add(this.tBxGreen);
-            this.groupBox1.Controls.Add(this.tBxred);
+            this.groupBox1.Controls.Add(this.tBxRed);
             this.groupBox1.Controls.Add(this.label6);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.label4);
@@ -132,33 +133,33 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Selected first Color";
             // 
-            // panel1
+            // pnlSelectedScreen1
             // 
-            this.panel1.Location = new System.Drawing.Point(193, 29);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(101, 106);
-            this.panel1.TabIndex = 9;
+            this.pnlSelectedScreen1.Location = new System.Drawing.Point(193, 29);
+            this.pnlSelectedScreen1.Name = "pnlSelectedScreen1";
+            this.pnlSelectedScreen1.Size = new System.Drawing.Size(101, 106);
+            this.pnlSelectedScreen1.TabIndex = 9;
             // 
             // tBxBlue
             // 
             this.tBxBlue.Location = new System.Drawing.Point(80, 105);
             this.tBxBlue.Name = "tBxBlue";
-            this.tBxBlue.Size = new System.Drawing.Size(107, 26);
+            this.tBxBlue.Size = new System.Drawing.Size(107, 30);
             this.tBxBlue.TabIndex = 8;
             // 
             // tBxGreen
             // 
             this.tBxGreen.Location = new System.Drawing.Point(80, 69);
             this.tBxGreen.Name = "tBxGreen";
-            this.tBxGreen.Size = new System.Drawing.Size(107, 26);
+            this.tBxGreen.Size = new System.Drawing.Size(107, 30);
             this.tBxGreen.TabIndex = 7;
             // 
-            // tBxred
+            // tBxRed
             // 
-            this.tBxred.Location = new System.Drawing.Point(80, 33);
-            this.tBxred.Name = "tBxred";
-            this.tBxred.Size = new System.Drawing.Size(107, 26);
-            this.tBxred.TabIndex = 6;
+            this.tBxRed.Location = new System.Drawing.Point(80, 33);
+            this.tBxRed.Name = "tBxRed";
+            this.tBxRed.Size = new System.Drawing.Size(107, 30);
+            this.tBxRed.TabIndex = 6;
             // 
             // label6
             // 
@@ -166,7 +167,7 @@
             this.label6.Font = new System.Drawing.Font("Sitka Display", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label6.Location = new System.Drawing.Point(6, 98);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(38, 23);
+            this.label6.Size = new System.Drawing.Size(48, 29);
             this.label6.TabIndex = 5;
             this.label6.Text = "Blue";
             this.label6.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -177,7 +178,7 @@
             this.label5.Font = new System.Drawing.Font("Sitka Display", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label5.Location = new System.Drawing.Point(6, 62);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(48, 23);
+            this.label5.Size = new System.Drawing.Size(63, 29);
             this.label5.TabIndex = 4;
             this.label5.Text = "Green";
             this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -188,7 +189,7 @@
             this.label4.Font = new System.Drawing.Font("Sitka Display", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label4.Location = new System.Drawing.Point(10, 28);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(34, 23);
+            this.label4.Size = new System.Drawing.Size(44, 29);
             this.label4.TabIndex = 3;
             this.label4.Text = "Red";
             this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -210,25 +211,32 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Selected second Color";
             // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(188, 27);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(106, 106);
+            this.panel2.TabIndex = 10;
+            // 
             // tBxBlue1
             // 
             this.tBxBlue1.Location = new System.Drawing.Point(75, 99);
             this.tBxBlue1.Name = "tBxBlue1";
-            this.tBxBlue1.Size = new System.Drawing.Size(107, 26);
+            this.tBxBlue1.Size = new System.Drawing.Size(107, 30);
             this.tBxBlue1.TabIndex = 8;
             // 
             // tBxGreen1
             // 
             this.tBxGreen1.Location = new System.Drawing.Point(75, 63);
             this.tBxGreen1.Name = "tBxGreen1";
-            this.tBxGreen1.Size = new System.Drawing.Size(107, 26);
+            this.tBxGreen1.Size = new System.Drawing.Size(107, 30);
             this.tBxGreen1.TabIndex = 7;
             // 
             // tBxRed1
             // 
             this.tBxRed1.Location = new System.Drawing.Point(75, 27);
             this.tBxRed1.Name = "tBxRed1";
-            this.tBxRed1.Size = new System.Drawing.Size(107, 26);
+            this.tBxRed1.Size = new System.Drawing.Size(107, 30);
             this.tBxRed1.TabIndex = 6;
             // 
             // label7
@@ -237,7 +245,7 @@
             this.label7.Font = new System.Drawing.Font("Sitka Display", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label7.Location = new System.Drawing.Point(6, 98);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(38, 23);
+            this.label7.Size = new System.Drawing.Size(48, 29);
             this.label7.TabIndex = 5;
             this.label7.Text = "Blue";
             this.label7.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -248,7 +256,7 @@
             this.label8.Font = new System.Drawing.Font("Sitka Display", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label8.Location = new System.Drawing.Point(6, 62);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(48, 23);
+            this.label8.Size = new System.Drawing.Size(63, 29);
             this.label8.TabIndex = 4;
             this.label8.Text = "Green";
             this.label8.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -259,17 +267,10 @@
             this.label9.Font = new System.Drawing.Font("Sitka Display", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.label9.Location = new System.Drawing.Point(10, 28);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(34, 23);
+            this.label9.Size = new System.Drawing.Size(44, 29);
             this.label9.TabIndex = 3;
             this.label9.Text = "Red";
             this.label9.TextAlign = System.Drawing.ContentAlignment.TopCenter;
-            // 
-            // panel2
-            // 
-            this.panel2.Location = new System.Drawing.Point(188, 27);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(106, 106);
-            this.panel2.TabIndex = 10;
             // 
             // groupBox3
             // 
@@ -283,15 +284,6 @@
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Mixed Result";
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(19, 75);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(95, 25);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Mix Color";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // panel3
             // 
             this.panel3.Location = new System.Drawing.Point(133, 25);
@@ -299,9 +291,18 @@
             this.panel3.Size = new System.Drawing.Size(124, 95);
             this.panel3.TabIndex = 11;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Mix Color";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(925, 622);
             this.Controls.Add(this.groupBox3);
@@ -336,10 +337,10 @@
         private Label label3;
         private PictureBox pictureBox2;
         private GroupBox groupBox1;
-        private Panel panel1;
+        private Panel pnlSelectedScreen1;
         private MaskedTextBox tBxBlue;
         private MaskedTextBox tBxGreen;
-        private MaskedTextBox tBxred;
+        private MaskedTextBox tBxRed;
         private Label label6;
         private Label label5;
         private Label label4;
--- colorMixer_App/Form1.cs
+++ colorMixer_App/Form1.cs
@@ -6,6 +6,16 @@ namespace colorMixer_App
         {
             InitializeComponent();
         }
+     public void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Bitmap pixelData = (Bitmap)pictureBox1.Image;
+            Color clr = pixelData.GetPixel(e.X, e.Y);
+            tBxRed.Text = clr.R.ToString();
+            tBxGreen.Text = clr.G.ToString();
+            tBxBlue.Text = clr.B.ToString();
+            pnlSelectedScreen1.BackColor = clr;
+
+        }
 
        
     }
