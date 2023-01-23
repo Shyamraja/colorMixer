c48e746e3f3b73780774243aac89a1c7a50e8810
Shyam <rsambhetwal@gmail.com>
2023-01-23 16:50:58 +0200

    Function updated for mixing colors and displaying mixed result

--- colorMixer_App/Form1.cs
+++ colorMixer_App/Form1.cs
@@ -30,14 +30,20 @@ namespace colorMixer_App
         private void mixSelectedColors(object sender, EventArgs e)
         {
             Color mix;
-            int r1, g1, b1, r2, g2, b2;
+            int r1, g1, b1, r2, g2, b2, r3, g3, b3;
             r1 = Convert.ToInt16(tBxRed.Text);
             r2 = Convert.ToInt16(tBxRed1.Text);
             g1 = Convert.ToInt16(tBxGreen.Text);
             g2 = Convert.ToInt16(tBxGreen1.Text);
             b1 = Convert.ToInt16(tBxBlue.Text);
             b2 = Convert.ToInt16(tBxBlue1.Text);
-            mix = Color.FromArgb(r1, g2, b1,b2);
+            r3 = Math.Min((r1 + r2), 255);
+            g3 = Math.Min((g1 + g2), 255);  
+            b3 = Math.Min((b1 + b2), 255);
+            //return mix(Convert.ToByte(r3), Convert.ToByte(g3), Convert.ToByte(b3));
+
+            //mix = Color.FromArgb(r3, g3, b3);
+            mix = Color.FromArgb(r1, g2, b1, b2);
             panelMixedColor.BackColor = mix;
         }
     }
